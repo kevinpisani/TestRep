c16e48006d681d91477c9d3b1d6fb043966a8b46
kevinpisani <kpisani@winssystems.com>
2014-10-22 11:29:44 +0200

    copy and paste test

--- abtermcall.php
+++ abtermcall.php
@@ -20,6 +20,10 @@
   require($abtermcalls . "/abtermcallresult.php");
   echo "</div>";
 
+  echo "<div id='graphDisplayBox6'>";
+  require($abtermcalls . "/abtermcallresult.php");
+  echo "</div>";
+
 require($incFolderPath . "/foot.php");
 
 ?>
