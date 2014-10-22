d0cdf38c0c53ec7bc270947b46d4251e2708d0dc
kevinpisani <kpisani@winssystems.com>
2014-10-22 12:14:50 +0200

    new tool

---
+++ test2.php
@@ -0,0 +1,29 @@
+<?php
+
+  $pageName ="BTS List";
+
+  require("globals.php");
+  include ($tools . "/winsDbConn.php");
+  include ($tools . "/sendemailsms_mime_v2.php");
+  require($incFolderPath . "/generic.php");
+  require($incFolderPath . "/head.php");
+  
+  echo "<div id='graphDisplayBox6'>";
+  require($abtermcalls . "/abtermcallresult.php");
+  echo "</div>";
+
+  require("globals.php");
+  include ($tools . "/winsDbConn.php");
+  include ($tools . "/sendemailsms_mime_v2.php");
+  require($incFolderPath . "/generic.php");
+  require($incFolderPath . "/head.php");
+  
+  echo "<div id='graphDisplayBox6'>";
+  require($abtermcalls . "/abtermcallresult.php");
+  echo "</div>";
+
+require($incFolderPath . "/foot.php");
+
+?>
+
+ 
