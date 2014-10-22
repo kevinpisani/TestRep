b5db682443c603c8028f81a33a513f9c11a65042
kevinpisani <kpisani@winssystems.com>
2014-10-22 12:12:55 +0200

    Revert "copy and paste test"
    This reverts commit c16e48006d681d91477c9d3b1d6fb043966a8b46.

--- abtermcall.php
+++ abtermcall.php
@@ -12,10 +12,6 @@
   require($abtermcalls . "/abtermcallresult.php");
   echo "</div>";
 
-  echo "<div id='graphDisplayBox6'>";
-  require($abtermcalls . "/abtermcallresult.php");
-  echo "</div>";
-
 require($incFolderPath . "/foot.php");
 
 ?>
